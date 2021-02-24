b97d3e50b350497b198ccc838a12247e5a6dab9d
Pinova Elektronik Mühendislik <66210166+Pinova-hub@users.noreply.github.com>
2021-02-24 14:23:00 +0300

    Update 5_SV2_ARD_BUTONLA_SIRAYLA_LED_YAKMA.ino

--- 5_SV2_ARD_BUTONLA_SIRAYLA_LED_YAKMA/5_SV2_ARD_BUTONLA_SIRAYLA_LED_YAKMA.ino
+++ 5_SV2_ARD_BUTONLA_SIRAYLA_LED_YAKMA/5_SV2_ARD_BUTONLA_SIRAYLA_LED_YAKMA.ino
@@ -1,37 +1,38 @@
   
-#define BUTON 5
+#define BUTON 5                                             //BUTON değşkeni ve bağlı olduğu pin tanımlanır
 
-int Led [ ] = {22,23,24,25,26,27,28,29,30,31};
+int Led [ ] = {22,23,24,25,26,27,28,29,30,31};              //10 adet LED ve bağlı olduğu pinler, Led değişkeni ile dizi şeklinde tanımlanır
 
-int Buton_durumu=1;
+int Buton_durumu=1;                                         //Buton_durumu tanımlanır ve butonun başlangıçtaki dijital durumu değişkene atanır(Butonlar pull-up bağlıolduğundan başlangıçtaki dijital udurmları 1'dir. Pull-down buton kullanılması durumunda başlangıçtaki dijital durumu 0 olarak girilmelidir)
+  
 
 
 void setup() {
-  
- pinMode(BUTON,INPUT);
+    
+ pinMode(BUTON,INPUT);                                      //BUTON'a giriş verilir                   
                                         
-    for(int i=0; i<10; i++) 
+    for(int i=0; i<10; i++)                                 // i değişkeni ve başlangıç değeri tanımlanır. i değişkeni 0'dan başlayarak her seferinde 1 artırılır ve en son 9 değerini alıncaya kadar for döngüsü tekrar edilir. 
     
     {
-    pinMode(Led [i], OUTPUT);
+    pinMode(Led [i], OUTPUT);                               //10 adet LED'e i değişkeni kullanılarak çıkış verilir
     }
   
 }
  
 void loop() {
 
- Buton_durumu = digitalRead(BUTON);
+ Buton_durumu = digitalRead(BUTON);                         //Butona basılı olup olmadığı dijital olarak okunarak kontrol edilir ve durumu Buton_durumu değişkenine atanır
  
-    if (Buton_durumu == 0)
+    if (Buton_durumu == 0)                                  //Buton basılıysa
     
     {  
-        for (int Sayici =0; Sayici<10; Sayici++)
+        for (int Sayici =0; Sayici<10; Sayici++)            //Sayici değişkeni ve başlangıç değeri tanımlanır. Sayici değişkeni 0'dan başlayarak her seferinde 1 artırılır ve en son 9 değerini alıncaya kadar for döngüsü tekrar edilir. 
         
         {
-        digitalWrite(Led [Sayici], HIGH);               
-        delay(500);
-        digitalWrite(Led [Sayici], LOW);
-        delay (500);
+        digitalWrite(Led [Sayici], HIGH);                  //Sayici değişkeni yardımıyla 10 adet LED'e sırayla dijital 1 (5V) verilir, LED yakılır           
+        delay(500);                                        //500 milisaniye bekleme süresi eklenir
+        digitalWrite(Led [Sayici], LOW);                   //Sayici değişkeni yardımıyla 10 adet LED'e sırayla dijital 0 (0V) verilir, LED söndürülür
+        delay (500);                                       //500 milisaniye bekleme süresi eklenir
         }
         
     }
